8e2541df5fca150b0e4dd66583fc5f9653e15869
Vishal Vishwakarma <vishal@vishalvishwakarma.com>
2018-05-06 19:16:30 +0530

    rftggr

--- public/wp-config.php
+++ public/wp-config.php
@@ -26,10 +26,10 @@ define('DB_NAME', 'default');
 define('DB_USER', 'laravel');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'GXGdpvI4gBLvPmSS');
+define('DB_PASSWORD', getenv("DB_PASSWORD"));
 
 /** MySQL hostname */
-define('DB_HOST', '172.30.154.114');
+define('DB_HOST', getenv(strtoupper(getenv("DB_SERVICE_NAME"))."_SERVICE_HOST"));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
