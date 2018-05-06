94ba8d201bc7550781effe339781971fdff32ffc
Vishal Vishwakarma <vishal@vishalvishwakarma.com>
2018-05-06 18:36:23 +0530

    19
    Signed-off-by: Vishal Vishwakarma <vishal@vishalvishwakarma.com>

--- public/wp-config.php
+++ public/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'default');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'laravel');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'GXGdpvI4gBLvPmSS');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '172.30.154.114');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
