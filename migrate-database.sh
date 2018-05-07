40f9095d463101902283951aa92d5d40d59b452a
Vishal Vishwakarma <vishal@vishalvishwakarma.com>
2018-05-07 16:43:11 +0530

    Signed-off-by: Vishal Vishwakarma <vishal@vishalvishwakarma.com>

--- migrate-database.sh
+++ migrate-database.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 echo "Migrating database 'php artisan migrate --force'..."
-sudo chown -R default.default /opt/app-root/src/public
+chown -R default.default /opt/app-root/src/public
